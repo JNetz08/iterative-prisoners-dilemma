0c09dfc78989dbf92e66ffa7ebfef6fed5ff3a8d
JNetz08 <37628482+JNetz08@users.noreply.github.com>
2018-03-22 10:26:17 -0400

    ChangedTeamName2

--- team4.py
+++ team4.py
@@ -1,6 +1,6 @@
 ####
 # Each team's file must define four tokens:
-#     team_name: J
+#     team_name: JCM
 #     strategy_name: a string
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
